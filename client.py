0c4bb2e8cd41ba6f8c5be8f0ae331dc7bf9b5fad
Everton <everton.aga88@gmail.com>
2023-07-05 21:38:37 +0100

    update with Custom Tkinter

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import threading
 from tkinter import *
 import tkinter
 from tkinter import simpledialog
+import customtkinter
 
 class Chat:
     def __init__(self):
@@ -26,24 +27,27 @@ class Chat:
         self.window()
         
     def window(self):
-        self.root = Tk()
+        self.root = customtkinter.CTk()
         self.root.geometry('800x800')
         self.root.title('Chat Group')
+        self.root._set_appearance_mode('dark')
+    
         
-        self.text_box = Text(self.root) #onde se localiza:
-        self.text_box.place(relx=0.05, rely=0.05, width=700,height=600)
+        self.text_box = customtkinter.CTkTextbox(self.root, width=700,height=600,corner_radius=10,border_color='blue') #onde se localiza:
+        self.text_box.place(relx=0.05, rely=0.05, )
         
         self.send_message = Entry(self.root) #onde se localiza o botao
         self.send_message.place(relx=0.05, rely=0.85, width=500,height=30)
         
-        self.btn_send = Button(self.root, text="Enviar", command=self.enviar_mensagem)
-        self.btn_send.place(relx=0.70, rely=0.85, width=50,height=30)
+        self.btn_send = customtkinter.CTkButton(self.root, text="Enviar",command=self.enviar_mensagem, width=50,height=30, hover_color=('green'), font=('', 20),corner_radius=2)
+        self.btn_send.place(relx=0.70, rely=0.85)
         self.root.protocol("WM_DELETE_WINDOW", self.close)
         
         self.root.mainloop()#faz a janela ficar aberta
         
     def close(self):  #fechar quando clicar
         self.root.destroy()
+        self.root.quit
         self.client.close()
         
         
